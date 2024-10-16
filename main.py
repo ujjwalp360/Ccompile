cca1ec8652dd29ad072df6783a02618051d045cf
ujjwalp360 <145079274+ujjwalp360@users.noreply.github.com>
2024-10-16 10:05:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,12 @@ from streamlit_ace import st_ace
 # Title of the app
 st.title("Online C Compiler")
 
-# Default C code snippet
-default_code = """// Online C compiler to run C program online
+# Sidebar for selecting different practical experiments
+st.sidebar.title("Practical Selection")
+experiment = st.sidebar.selectbox("Select Practical Experiment", ("Home", "Experiment 1", "Experiment 2", "Experiment 3"))
+
+# Default code for each experiment
+home_code = """// Online C compiler to run C program online
 #include <stdio.h>
 
 int main() {
@@ -16,6 +20,66 @@ int main() {
     return 0;
 }
 """
+
+exp1_code = """// Experiment 1: Addition of Two Numbers
+#include <stdio.h>
+
+int main() {
+    int a, b, sum;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum: %d\\n", sum);
+    return 0;
+}
+"""
+
+exp2_code = """// Experiment 2: Find the Largest of Three Numbers
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a >= b && a >= c)
+        printf("%d is the largest number\\n", a);
+    else if (b >= a && b >= c)
+        printf("%d is the largest number\\n", b);
+    else
+        printf("%d is the largest number\\n", c);
+    return 0;
+}
+"""
+
+exp3_code = """// Experiment 3: Calculate the Factorial of a Number
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    unsigned long long factorial = 1;
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+    else {
+        for (i = 1; i <= n; ++i) {
+            factorial *= i;
+        }
+        printf("Factorial of %d = %llu\\n", n, factorial);
+    }
+    return 0;
+}
+"""
+
+# Determine the default code based on the selected experiment
+if experiment == "Home":
+    default_code = home_code
+elif experiment == "Experiment 1":
+    default_code = exp1_code
+elif experiment == "Experiment 2":
+    default_code = exp2_code
+elif experiment == "Experiment 3":
+    default_code = exp3_code
 
 # Ace editor for C code input with auto-indentation and auto-closing of quotes, braces, etc.
 code = st_ace(language='c', theme='monokai', auto_update=True, keybinding="vscode", height=300, value=default_code)
@@ -62,4 +126,3 @@ if st.button("Compile and Run"):
             st.text(compile_result.stderr.decode("utf-8"))
     else:
         st.warning("Please write some C code.")
-        
